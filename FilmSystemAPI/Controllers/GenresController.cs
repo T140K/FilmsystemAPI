abc29671600908f971f758e2eeaf3afe107adf74
Darek <darek1430143@gmail.com>
2023-06-02 05:31:47 +0200

    added a httpsget to get all genres form the db

---
+++ FilmSystemAPI/Controllers/GenresController.cs
@@ -0,0 +1,34 @@
+﻿using FilmSystemAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private readonly FilmSystemContext _context;
+
+        public GenresController(FilmSystemContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetGenres")]
+        public async Task<ActionResult<List<Genres>>> GetGenresByPersonId()
+        {
+            var genres = await _context.Genre
+                .Select(g => new
+                {
+                    g.Id,
+                    g.Name,
+                    g.Description
+                })
+                .ToListAsync();
+
+            return Ok(genres);
+        }
+    }
+}
